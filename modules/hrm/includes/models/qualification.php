e1c5d6fad17438bdc132672f1e0dcc275da72be9
mattbarraud <matt@morvargh-sailing.co.uk>
2016-06-26 19:31:46 +0100

    Update qualification.php

--- modules/hrm/includes/models/qualification.php
+++ modules/hrm/includes/models/qualification.php
@@ -6,7 +6,7 @@ use WeDevs\ERP\Framework\Model;
  *
  * @package WeDevs\ERP\HRM\Models
  */
-class Qualifications extends Model {
+class Qualification extends Model {
     protected $table = 'erp_hr_qualification';
     protected $fillable = [ 'employee_id', 'qualification_provider', 'course_name', 'finished', 'notes' ];
 }
